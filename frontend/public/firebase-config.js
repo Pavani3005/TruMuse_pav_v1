d1a9384abec097358a7cdc3c43ab56b68f05547b
Pavani3005 <pavanigk7@gmail.com>
2025-08-05 09:59:47 +0530

    Duplicate repo

--- frontend/public/firebase-config.js
+++ frontend/public/firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     projectId: "trumuse-c2d53", // Replace with your actual project ID
     storageBucket: "trumuse-c2d53.firebasestorage.app", // Replace with your actual storage bucket
     messagingSenderId: "767023337153", // Replace with your actual sender ID
-    appId: "1:767023337153:web:47347632049ddd43848e40" // Replace with your actual app ID
+    appId: "1:767023337153:web:47347632049ddd43848e40",
 };
 
 // Export for use in other files
